8e45dec21eb9e7dd55308733b3c0b331a893f903
yceline-yu <cellystuff5@gmail.com>
2021-07-22 11:57:34 -0700

    first problem

---
+++ problems/reverseArray.js
@@ -0,0 +1,15 @@
+/** reverseArray
+ * 
+ * Given an array, return the reversed array
+ * 
+ * [1,2,3] => [3,2,1]
+ * [2,5] => [5,2]
+ */
+
+function reverseArray(arr) {
+  let reversed = [];
+  for (let i = a.length - 1; i >= 0; i--) {
+    reversed.push(a[i]);
+  }
+  return reversed;
+}
