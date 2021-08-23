17d84070cf54f855272cb7f881983faa5859e461
yceline-yu <cellystuff5@gmail.com>
2021-08-23 12:35:31 -0700

    mostWater

--- problems/mostWaterContainer.js
+++ problems/mostWaterContainer.js
@@ -5,7 +5,7 @@
  * Return the max area of water that can be contained from the "graph"
  * 
  * [1,1] => 1
- * [4,3,2,1,4] => 14
+ * [4,3,2,1,4] => 16
  * [1,2,1] => 2
  * [] => 0
  * [1,8,6,2,5,4,8,3,7] => 49
@@ -15,6 +15,8 @@
 // have totalLength value
 // area is (height x length) or 0
 // height ideally being the two highest points
+//    max height is the shorter of the two points
+//    height = ints[x] > ints[y] ? ints[y] : ints[x];
 // length is the space between the two heights
 //    how to calculate the space?
 //    subtract index? 8 - 1 = 7
@@ -34,12 +36,13 @@ function mostWaterContainer(ints) {
   let y = 0;
 
   while (x < totalLength) {
-    console.log("x,y", x,y);
-    currHeight = ints[y];
-    console.log("height", currHeight)
+    // console.log("x,y", x,y);
+    currHeight = ints[x] > ints[y] ? ints[y] : ints[x];
+    // console.log("height", currHeight)
     currLength = y - x;
-    console.log("length", currLength);
+    // console.log("length", currLength);
     currArea = currHeight * currLength;
+    // console.log("curARea", currArea);
 
     if (currArea > largestArea) {
       largestArea = currArea;
